7070bad8e421cb099b1c1f165420acad2f4bef55
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-17 16:22:36 +0200

    Create get items view

---
+++ pages/item/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-card>
+    <b-card-header>
+      <b-img src="https://troov.com/images/troov/logo-dark.png" fluid alt=""></b-img>
+      <h3>Bienvenue {{ userName }},</h3>
+      <p>Gérez de façon efficace vos objets</p>
+    </b-card-header>
+    <b-card-body>
+      <b-row class="justify-content-md-center" align-v="center" align-h="between">
+        <b-col md="4"></b-col>
+        <b-col md="8">
+          <b-table striped hover :items="items"></b-table>
+        </b-col>
+      </b-row>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+import getItemList from "~/pages/item/GetItemList";
+
+export default {
+  name: "Item",
+  async mounted() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      await this.$router.push("/");
+    }
+    this.items = await getItemList(this);
+  },
+  data() {
+    return {
+      userName: this.$route.query.name,
+      itemName: "",
+      itemDescription: "",
+      items: []
+    };
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
