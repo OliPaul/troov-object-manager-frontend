4f277f6906c8bc6353983220c7b7d7095d78cd8f
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-17 14:36:48 +0200

    Create login view with his implementation

---
+++ pages/authentication/login/Login.ts
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import User from "~/models/User";
+
+const processLoginForm = async (context: Vue, email: string, password: string): Promise<User | undefined> => {
+  if (email == "") {
+    alert("Veuillez saisir votre adresse email svp");
+    return;
+  }
+
+  if (password == "") {
+    alert("Veuillez saisir votre mot de passe svp");
+    return;
+  }
+
+  const userInfo = await context.$axios.$post("/sign_in", {email: email, password: password})
+  // Store token
+  if(userInfo.token) {
+    localStorage.setItem("token", userInfo.token);
+  }
+  return new User(userInfo.name, userInfo.email);
+}
+
+export default processLoginForm;
