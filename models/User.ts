64e5796da671926d5d65af559e44f3b5e33e8bd5
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-17 14:39:11 +0200

    Create user model

---
+++ models/User.ts
@@ -0,0 +1,19 @@
+export default class User {
+  private _name: string;
+  private _email: string;
+
+
+  constructor(name: string, email: string) {
+    this._name = name;
+    this._email = email;
+  }
+
+
+  get name(): string {
+    return this._name;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+}
