ed4a85aed17aa0515a13cc6f9876a4265012f3de
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-17 14:38:35 +0200

    Create main view

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <Tutorial />
+  <b-card>
+    <b-card-header>
+      <b-img src="https://troov.com/images/troov/logo-dark.png" fluid alt=""></b-img>
+      <p>Bienvenue, connectez-vous pour accéder à l'application.</p>
+    </b-card-header>
+    <b-card-body>
+      <Login />
+    </b-card-body>
+  </b-card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Login from "~/pages/authentication/login/index.vue";
 
 export default Vue.extend({
-  name: 'IndexPage'
+  components: {Login},
+  name: 'IndexPage',
+  methods: {}
 })
 </script>
