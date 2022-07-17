01d7ac7b1741e8f908129519f92564696dbd1b5f
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-17 16:16:37 +0200

    Use setHeader onRequest interceptor

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,15 +3,14 @@ export default function ({$axios, redirect}) {
     const token = localStorage.getItem("token");
 
     if (token) {
-      $axios.setToken(token, 'Bearer', ['post', 'put', 'get', 'delete']);
+      $axios.setHeader("Authorization", 'Bearer ' + token);
     }
   })
 
   $axios.onError(error => {
     if (error.response.status === 401) {
       redirect("/authentication/login");
-    }
-    if (error.response.status !== 200) {
+    } else if (error.response.status !== 200) {
       alert(error.response.data.error);
     }
     return new Promise(() => {
