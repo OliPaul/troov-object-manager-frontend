96119da513d031568364099dacba937b72365ccc
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-17 16:21:08 +0200

    Create item model

---
+++ models/Item.ts
@@ -0,0 +1,24 @@
+export default class Item {
+  private _id: string;
+  private _name: string;
+  private _description: string;
+
+
+  constructor(id: string, name: string, description: string) {
+    this._id = id;
+    this._name = name;
+    this._description = description;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+}
